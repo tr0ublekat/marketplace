ed332713770904b30ee1ba5572657204a2f33fb9
tr0ublekat <kirillkojev8@gmail.com>
2025-07-17 10:48:12 +0300

    add universal bind to queues

--- go-esb/app/main.go
+++ go-esb/app/main.go
@@ -6,58 +6,61 @@ import (
 	"github.com/streadway/amqp"
 )
 
+func failOnError(msg string, err error) {
+	if err != nil {
+		fmt.Printf("%s: %s\n", msg, err)
+	}
+}
+
+func handleOrderCreated(body []byte) {
+	fmt.Printf("Заказ создан: %s\n", string(body))
+}
+
 func main() {
 	conn, err := amqp.Dial("amqp://rmq_admin:rmq_password@localhost/")
-
-	if err != nil {
-		fmt.Println("Ошибка подключения к RabbitMQ:", err)
-		return
-	}
+	failOnError("Ошибка подключения к RabbitMQ:", err)
 	defer conn.Close()
 
 	ch, err := conn.Channel()
-	if err != nil {
-		fmt.Println("Ошибка создания канала:", err)
-		return
-	}
+	failOnError("Ошибка открытия канала:", err)
 	defer ch.Close()
 
 	err = ch.ExchangeDeclare(
-		"events",
-		"topic",
-		false, // durable
-		false, // delete when unused
-		false, // exclusive
-		false, // no-wait
-		nil,   // аргументы
+		"marketplace", // имя exchange
+		"direct",      // тип exchange
+		false,         // durable
+		false,         // delete when unused
+		false,         // exclusive
+		false,         // no-wait
+		nil,           // аргументы
 	)
-	if err != nil {
-		fmt.Println("Ошибка создания обмена:", err)
-		return
-	}
+	failOnError("Ошибка создания exchange:", err)
 
 	q, err := ch.QueueDeclare(
-		"order_events_queue", // имя очереди
-		true,                 // durable
-		false,                // delete when unused
-		false,                // exclusive
-		false, nil,
-	)
-	if err != nil {
-		fmt.Println("Ошибка создания очереди:", err)
-		return
-	}
-
-	err = ch.QueueBind(
-		q.Name,          // имя очереди
-		"order.created", // routing key
-		"events",        // имя exchange
+		"ebs_queue", // имя очереди
+		true,        // durable
+		false,
+		false,
 		false,
 		nil,
 	)
-	if err != nil {
-		fmt.Println("Ошибка привязки очереди к exchange:", err)
-		return
+	failOnError("Ошибка создания очереди:", err)
+
+	routingKeys := []string{
+		"order.created",
+		"payment.success",
+		"delivery.sent",
+	}
+
+	for key := range routingKeys {
+		err = ch.QueueBind(
+			q.Name,           // имя очереди
+			routingKeys[key], // routing key
+			"marketplace",    // имя exchange
+			false,
+			nil,
+		)
+		failOnError(fmt.Sprintf("Ошибка привязки очереди к exchange с ключом %s:", routingKeys[key]), err)
 	}
 
 	msgs, err := ch.Consume(
@@ -69,12 +72,17 @@ func main() {
 		false,  // no-wait
 		nil,    // аргументы
 	)
-	if err != nil {
-		fmt.Println("Ошибка получения сообщений:", err)
-		return
-	}
+	failOnError("Ошибка подписки на очередь:", err)
+
+	fmt.Println("go-esb успешно запущен.")
 
 	for msg := range msgs {
-		fmt.Printf("Получено сообщение: %s\n", msg.Body)
+		switch msg.RoutingKey {
+		case "order.created":
+			handleOrderCreated(msg.Body)
+		default:
+			fmt.Printf("Неизвестное сообщение: %s\n", msg.RoutingKey)
+		}
 	}
+
 }
