ee300fbb00e248a6bdf753d60ad285681307f881
tr0ublekat <kirillkojev8@gmail.com>
2025-07-23 20:16:53 +0300

    edit params for db

--- orders/app/db.py
+++ orders/app/db.py
@@ -3,7 +3,14 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(str(DATABASE_URL), echo=False, pool_size=20, max_overflow=10)
+engine = create_async_engine(
+    str(DATABASE_URL),
+    echo=False,
+    pool_size=10,
+    max_overflow=5,
+    pool_timeout=30,
+    pool_recycle=1800,
+)
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
